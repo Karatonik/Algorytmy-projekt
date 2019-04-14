02c1986e1ef81884618701763f21b6b2ceb8a57b
John Doe <mat.kalksztejn@wp.pl>
2019-04-14 21:33:06 +0200

    Update EventsData.java

--- src/Firma/EventsData.java
+++ src/Firma/EventsData.java
@@ -8,11 +8,13 @@ public class EventsData {
     private final StringProperty name_Event;
     private final StringProperty Date;
     private final StringProperty ID_Firmy;
-    public EventsData(String id,String naz,String dob,String fir){
+    private final StringProperty pw;
+    public EventsData(String id,String naz,String dob,String fir,String pw){
         this.ID_Event=new SimpleStringProperty(id);
         this.name_Event=new SimpleStringProperty(naz);
         this.Date=new SimpleStringProperty(dob);
         this.ID_Firmy=new SimpleStringProperty(fir);
+        this.pw=new SimpleStringProperty(pw);
     }
 
     public String getID_Event() {
@@ -61,5 +63,17 @@ public class EventsData {
 
     public void setID_Firmy(String ID_Firmy) {
         this.ID_Firmy.set(ID_Firmy);
+    }
+
+    public String getPw() {
+        return pw.get();
+    }
+
+    public StringProperty pwProperty() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw.set(pw);
     }
 }
