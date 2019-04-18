3e12e8014c014e2e913c2b8bf2feb781c92f3ec4
John Doe <mat.kalksztejn@wp.pl>
2019-04-18 18:19:44 +0200

    Update WorkersControler.java

--- src/Pracownik/WorkersControler.java
+++ src/Pracownik/WorkersControler.java
@@ -1,15 +1,22 @@
 package Pracownik;
+
 import dbUtil.dbConnection;
-import java.net.URL;
-import java.sql.*;
-import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 import static Loginapp.LoginControler.namef;
 
