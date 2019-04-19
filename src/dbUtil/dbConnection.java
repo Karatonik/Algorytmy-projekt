8fb04083e1d09a1b1e381bffad28aecf9ee45d07
John Doe <mat.kalksztejn@wp.pl>
2019-04-19 18:57:43 +0200

    Update dbConnection.java

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -9,7 +9,7 @@ public class dbConnection {
     private static final String PASS = "password";
     private static final String CONN = "jdbc:mysql://localhost/login";
     private static final String SQCONN = "jdbc:sqlite:test.sqlite";
-
+//uzyskanie połącznia z baza danych
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("org.sqlite.JDBC");
